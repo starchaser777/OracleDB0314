dc2ca2a62a2a55f147957036def1e3ba40d0b5c9
starchaser777 <les980730@gmail.com>
2024-03-21 11:56:09 +0900

    testPageDefault()

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -5,6 +5,9 @@ import kr.ac.kopo.oracledb0314.entity.Memo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -77,5 +80,19 @@ public class MemoRepositoryTests {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault() {
+        // 1페이지당 10개의 Entity
+        Pageable pageable = PageRequest.of(1, 10);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        System.out.println(result);
+
+        for (Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
     }
 }
